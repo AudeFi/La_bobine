38ac618c51d80eecfa86cf1491f9789279742346
maxaloc <maxaloc@gmail.com>
2016-04-12 19:45:45 +0200

    LOL

--- views/pages/disconnect.php
+++ views/pages/disconnect.php
@@ -1,8 +1,17 @@
 <?php
-	
+
 	if (isset($_SESSION)){
 		session_destroy();
 	}
+	//header('Location:' . URL .'connexion');
+	//header('Location:'.URL.'index.php');
+	//header('Location: ../../index.php');
+	//exit();
+?>
+
+<script>
+	window.location.href= '../../index.php';
+
+</script>
+
 	
-	header('Location:' . URL .'connexion');
-	exit;
--- views/pages/home.php
+++ views/pages/home.php
@@ -3,29 +3,44 @@
 	$query = $pdo->query('SELECT * FROM musics');
 	$playlist = $query->fetchAll();
 
-	echo '<pre>';
-	print_r($playlist);
-	echo '</pre>';
+	// echo '<pre>';
+	// print_r($playlist);
+	// echo '</pre>';
 
 	$result = count($playlist);
 	$id = rand(0, $result - 1);
 
-	echo $id;
+	// echo $id;
 
 	$music = $playlist[$id];
 
-	echo '<pre>';
-	print_r($music);
-	echo '</pre>';
+	// echo '<pre>';
+	// print_r($music);
+	// echo '</pre>';
+
+	if(!empty(['name']))
+	{
+		$name = strip_tags(trim($_POST['name']));
+		$name=urlencode($name);
+
+		$ch = curl_init();
+		
+		curl_setopt($ch, CURLOPT_URL, "http://api.themoviedb.org/3/search/movie?api_key=2fb5cd2aa5d0d9868fcd75aba6d96451&query='.$name.'");
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($ch, CURLOPT_HEADER, FALSE);
+		
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		  "Accept: application/json"
+		));
+		
+		$response = curl_exec($ch);
+		curl_close($ch);
+		
+		$result = json_decode($response);
+
+		$count = count($result->results);
 
 ?>
-
-
-
-
-
-
-
 
 <section>
 	
