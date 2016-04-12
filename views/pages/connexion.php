d8ed0a04c2c613a63c5de2363a12cebc634e5aef
AudeFi <odefi@hotmail.fr>
2016-04-12 11:10:39 +0200

    creation form-login
    details dans le formulaire de connexion

--- views/pages/connexion.php
+++ views/pages/connexion.php
@@ -1,43 +1,6 @@
 <?php
 
-
-	if(!empty($_POST['loginform'])) 
-	{
-		$email = $_POST['email'];
-		$password = hash('sha256',SALT.$_POST['password']);
-
-		$prepare = $pdo->prepare('SELECT * FROM users WHERE email = :email');
-		$prepare->bindValue('email',$email);
-		$execute = $prepare->execute();
-		$user = $prepare->fetch();
-
-		if($user){
-			if($user->password == $password) {
-				$_SESSION['user'] = array(
-					'id' => $user->id,
-					'email' => $user->email
-				);
-			}
-			else {
-				echo "Nop";
-			}
-		}
-		else {
-			echo "Login ou mot de passe incorrect";
-		}
-	}
-
-
-	if(!empty($_POST['signinform'])) {
-		echo "Merci de votre inscription";
-		$email = $_POST['email'];
-		$password = hash('sha256',SALT.$_POST['password']);
-
-		$prepare = $pdo->prepare('INSERT INTO users (email,password) VALUES (:email,:password)');
-		$prepare->bindValue('email',$email);
-		$prepare->bindValue('password',$password);
-		$execute = $prepare->execute();
-	}
+	require 'config/form-login.php';
 
 ?>
 
@@ -67,6 +30,10 @@
 	</form>
 
 <?php endif; ?>
+
+
+
+
 
 	<h2>Ou inscrivez-vous</h2>
 
