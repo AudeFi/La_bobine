cfe7f451e28353371006cac0767826212d9615ff
maxaloc <maxaloc@gmail.com>
2016-04-12 18:52:37 +0200

    Home

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,5 +1,54 @@
+<?php
+
+	$query = $pdo->query('SELECT * FROM musics');
+	$playlist = $query->fetchAll();
+
+	echo '<pre>';
+	print_r($playlist);
+	echo '</pre>';
+
+	$result = count($playlist);
+	$id = rand(0, $result - 1);
+
+	echo $id;
+
+	$music = $playlist[$id];
+
+	echo '<pre>';
+	print_r($music);
+	echo '</pre>';
+
+?>
+
+
+
+
+
+
+
+
 <section>
 	
-	Home
+
+
+
+	<iframe class="ytvideo" width="560" height="315" src="<?= $music->link ?>" frameborder="0" allowfullscreen>
+		
+	</iframe>
+
+	<script>
+		var video = document.querySelector('.ytvideo');
+		var url = video.src;
+		url = url.replace("watch?v=", "v/");
+		video.src = url;
+
+	</script>
+
+	<p>Title : <?= $music->title ?></p>
+	<p>Composer : <?= $music->composer ?></p>
+
+
+
+
 
 </section>
