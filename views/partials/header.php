3a5347aa3952c234d3e52ca5d47079a0ab71cbb0
AudeFi <odefi@hotmail.fr>
2016-04-13 11:03:17 +0200

    header

--- views/partials/header.php
+++ views/partials/header.php
@@ -2,11 +2,35 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
 	<title><?= $title ?></title>
-	<!-- <link rel="stylesheet" href="<?= URL ?>src/css/reset.css"> -->
+	<link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="<?= URL ?>src/css/reset.css">
 	<link rel="stylesheet" href="<?= URL ?>src/css/style.css">
 	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
 </head>
 <body class="page-<?= $class ?>">
 
-	<div class="container">
+	<div class="container-global">
+			<nav>
+				<div class="menu">
+					<div class="menu-logo">
+						<img src="<?= URL ?>src/images/logo.svg" alt="logo">
+					</div>
+					<div class="menu-content">
+						<ul>
+							<li><a href="<?= URL ?>">parcourir</a></li>
+							<li><a href="<?= URL ?>playlists">playlists</a></li>
+							<li><a href="<?= URL ?>add-musics">ajoutez votre musique</a></li>
+							<a href="<?= URL ?>connexion"><li>connexion</li></a>
+						</ul>
+					</div>
+					<div class="menu-social">
+						<ul>
+							<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</nav>
