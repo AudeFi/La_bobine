00659d395fb9fa551a29f5b4ae4aaf42956f2e7c
AudeFi <odefi@hotmail.fr>
2016-04-13 21:07:59 +0200

    front admin à peu près
    Front de la page admin a peu près correcte

--- views/pages/admin.php
+++ views/pages/admin.php
@@ -58,96 +58,29 @@
 
 		<div class="admin-part approbation">
 			<h3>Approuvez les demandes de musiques</h3>
-			<table>
-			 	<tr>
-			 		<th>ID</th>
-			 		<th>Movie</th>
-			 		<th>Movie ID</th>
-					<th>Music Title</th>
-		 			<th>Composer</th>
-		 			<th>Music Link</th>
-			 		<th>Validation</th>
-			 		<th>Erase</th>
-			 	</tr>
+			<div class="scroll">
 			 	<?php foreach($validation_musics as $_validation): ?>
-			 	<tr>
-			 		<td><?= $_validation->id ?></td>
-			 		<td><?= $_validation->movie_name ?></td>
-			 		<td><?= $_validation->movie_id ?></td>
-			 		<td><?= $_validation->music_title ?></td>
-			 		<td><?= $_validation->composer ?></td>
-			 		<td><?= $_validation->music_link ?></td>
-			 		<td>
-						<form class="check" action="config/validation.php?id=<?= $_validation->id; ?>" method="POST">
-			 				<button class="validate" type="submit" value="VALIDE" name="validate"></button>
-			 			</form>
-			 		</td>
-			 		<td>
-			 			<form class="trash" action="config/delete.php?id=<?= $_validation->id; ?>" method="POST">
-			 				<button class="erase" type="submit" value="REMOVE" name="erase"></button>
-			 			</form>
-			 		</td>
-			 	</tr>
+			 	<div class="line">
+				 	<div class="infos">
+				 		<div class="first-line">
+				 			<div class="titles"><?= $_validation->music_title ?> - <?= $_validation->composer ?></div>
+				 			<div class="link"><?= $_validation->music_link ?></div>
+				 		</div>
+				 		<div class="second-line">
+				 			<div class="backline"><?= $_validation->movie_name ?></div>
+						</div>	
+					</div>
+					<div class="buttons">
+							<form class="check" action="config/validation.php?id=<?= $_validation->id; ?>" method="POST">
+				 				<button class="validate" type="submit" value="VALIDE" name="validate">ACCEPTER</button>
+				 			</form>
+				 			<form class="trash" action="config/delete.php?id=<?= $_validation->id; ?>" method="POST">
+				 				<button class="erase" type="submit" value="REMOVE" name="erase">REFUSER</button>
+				 			</form>
+				 	</div>
+			 	</div>
 			 	<?php endforeach; ?>
-			</table>
-
-<<<<<<< Updated upstream
-=======
-<table>
- 		<tr>
- 			<th>ID</th>
- 			<th>Movie</th>
- 			<th>Movie ID</th>
- 			<th>Music Title</th>
- 			<th>Composer</th>
- 			<th>Music Link</th>
- 			<th>Validation</th>
- 			<th>Erase</th>
- 		</tr>
- 		<?php foreach($validation_musics as $_validation): ?>
- 			<td><?= $_validation->id ?></td>
- 			<td><?= $_validation->movie_name ?></td>
- 			<td><?= $_validation->movie_id ?></td>
- 			<td><?= $_validation->music_title ?></td>
- 			<td><?= $_validation->composer ?></td>
- 			<td><?= $_validation->music_link ?></td>
- 			<td>
- 				<form class="check" action="config/validation.php?id=<?= $_validation->id; ?>" method="POST">
- 					<button class="validate" type="submit" value="VALIDE" name="validate">
- 					</button>
- 				</form>
- 			</td>
- 			<td>
- 				<form class="trash" action="config/delete.php?id=<?= $_validation->id; ?>" method="POST">
- 					<button class="erase" type="submit" value="REMOVE" name="erase">
- 					</button>
- 				</form>
- 			</td>
- 		</tr>
- 		<?php endforeach; ?>
- 	</table>
-
-
-<div class="add-playlists">
-	<h3>Ajoutez des musiques aux playlists</h3>
-	<form action="#" method="POST">
-		<div>
-			<h4>Sélectionnez une playlist</h4>
-			<?php foreach($all_playlists as $one_playlist): ?>
-				<label for="name_playlist<?=$one_playlist->id?>"><?= $one_playlist->name ?></label>
-				<input class="border" type="radio" value="<?= $one_playlist->id ?>" name="name_playlist" id="name_playlist<?=$one_playlist->id?>">
-			<?php endforeach; ?>
-		</div>
-		<div>
-			<h4>Sélectionnez des musiques</h4>
-			<?php foreach($all_musics as $one_music): ?>
-				<label for="name_music<?= $one_music->id ?>"><?= $one_music->music_title ?> par <?= $one_music->composer ?></label>
-				<input class="border" type="checkbox" value="<?= $one_music->id ?>" name="name_music[]" id="name_music<?= $one_music->id ?>">
-			<?php endforeach; ?>
-		</div>
-		<div>
-			<input type="submit" name="add-to-playlist">
->>>>>>> Stashed changes
+			</div>
 		</div>
 
 
@@ -157,23 +90,23 @@
 			<form action="#" method="POST">
 				<div class="flex">
 					<div class="colonne">
-						<h4>Selectionnez une playlist</h4>
-						<div class="scroll">
-							<?php foreach($all_playlists as $one_playlist): ?>
-							<div class="input-label">
-								<input class="border" type="radio" value="<?= $one_playlist->id ?>" name="name_playlist" id="name_playlist<?=$one_playlist->id?>">
-								<label for="name_playlist<?=$one_playlist->id?>"><?= $one_playlist->name ?></label>								
-							</div>
-							<?php endforeach; ?>
-						</div>
-					</div>
-					<div class="colonne">
 						<h4>Selectionnez des musiques</h4>
 						<div class="scroll">
 							<?php foreach($all_musics as $one_music): ?>
 							<div class="input-label">
 								<input class="border" type="checkbox" value="<?= $one_music->id ?>" name="name_music[]" id="name_music<?= $one_music->id ?>">
 								<label for="name_music<?= $one_music->id ?>"><?= $one_music->music_title ?> par <?= $one_music->composer ?></label>								
+							</div>
+							<?php endforeach; ?>
+						</div>
+					</div>
+					<div class="colonne">
+						<h4>Selectionnez une playlist</h4>
+						<div class="scroll">
+							<?php foreach($all_playlists as $one_playlist): ?>
+							<div class="input-label">
+								<input class="border" type="radio" value="<?= $one_playlist->id ?>" name="name_playlist" id="name_playlist<?=$one_playlist->id?>">
+								<label for="name_playlist<?=$one_playlist->id?>"><?= $one_playlist->name ?></label>								
 							</div>
 							<?php endforeach; ?>
 						</div>
@@ -191,41 +124,46 @@
 
 		<div class="admin-part list-playlists">
 			<h3>Voici le contenu des playlists</h3>
-			<div class="playlist-cat classiques">	
-				<h4>Les classiques</h4>
-				<ul>
-					<?php foreach($playlist1 as $content_playlist): ?>
-						<li><?= $content_playlist->movie_name ?></li>
-						<form class="delete_from_playlist" action="config/delete-from-playlist.php?id=<?= $content_playlist->id ?>&playlist=1" method="POST">
-		 					<button class="delete" type="submit" value="VALIDE" name="playlist1">
-		 					</button>
-		 				</form>
-					<?php endforeach; ?>
-				</ul>
-			</div>
-			<div class="playlist-cat bonne-humeur">	
-				<h4>Bonne humeur</h4>
-				<ul>
-					<?php foreach($playlist2 as $content_playlist): ?>
-						<li><?= $content_playlist->movie_name ?></li>
-						<form class="delete_from_playlist" action="config/delete-from-playlist.php?id=<?= $content_playlist->id ?>&playlist=2" method="POST">
-		 					<button class="delete" type="submit" value="VALIDE" name="playlist2">
-		 					</button>
-		 				</form>
-					<?php endforeach; ?>
-				</ul>
-			</div>
-			<div class="playlist-cat detente">	
-				<h4>Détente</h4>
-				<ul>
-					<?php foreach($playlist3 as $content_playlist): ?>
-						<li><?= $content_playlist->movie_name ?></li>
-						<form class="delete_from_playlist" action="config/delete-from-playlist.php?id=<?= $content_playlist->id ?>&playlist=3" method="POST">
-		 					<button class="delete" type="submit" value="VALIDE" name="playlist3">
-		 					</button>
-		 				</form>
-					<?php endforeach; ?>
-				</ul>
+			<div class="flex">
+				<div class="colonne classiques">	
+					<h4>Les classiques</h4>
+					<div class="scroll">
+						<?php foreach($playlist1 as $content_playlist): ?>
+							<div class="input-label">
+								<form class="delete_from_playlist" action="config/delete-from-playlist.php?id=<?= $content_playlist->id ?>&playlist=1" method="POST">
+				 					<button class="delete" type="submit" value="VALIDE" name="playlist1">X</button>
+				 				</form>
+				 				<span><?= $content_playlist->movie_name ?></span>
+				 			</div>
+						<?php endforeach; ?>
+					</div>
+				</div>
+				<div class="colonne bonne-humeur">	
+					<h4>Bonne humeur</h4>
+					<div class="scroll">
+						<?php foreach($playlist2 as $content_playlist): ?>
+							<div class="input-label">
+								<form class="delete_from_playlist" action="config/delete-from-playlist.php?id=<?= $content_playlist->id ?>&playlist=2" method="POST">
+				 					<button class="delete" type="submit" value="VALIDE" name="playlist2">X</button>
+				 				</form>
+				 				<span><?= $content_playlist->movie_name ?></span>
+				 			</div>
+						<?php endforeach; ?>
+					</div>
+				</div>
+				<div class="colonne detente">	
+					<h4>Détente</h4>
+					<div class="scroll">
+						<?php foreach($playlist3 as $content_playlist): ?>
+							<div class="input-label">
+								<form class="delete_from_playlist" action="config/delete-from-playlist.php?id=<?= $content_playlist->id ?>&playlist=3" method="POST">
+				 					<button class="delete" type="submit" value="VALIDE" name="playlist3">X</button>
+				 				</form>
+				 				<span><?= $content_playlist->movie_name ?></span>
+				 			</div>
+						<?php endforeach; ?>
+					</div>
+				</div>
 			</div>
 			
 		</div>
