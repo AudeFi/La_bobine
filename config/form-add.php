6cb3b1f22fb80359aab4b696525631f67d8a1621
maxaloc <maxaloc@gmail.com>
2016-04-13 14:02:26 +0200

    Rajout de la musique aléatoire + infos sur la home

--- config/form-add.php
+++ config/form-add.php
@@ -9,12 +9,12 @@ if(!empty($_POST['add'])){
 	$composer = '';
 
 	$music_title      	= strip_tags(trim($_POST['music_title']));
-	$movie_composer       = strip_tags(trim($_POST['composer']));
+	$composer     = strip_tags(trim($_POST['composer']));
 	$music_link       	= strip_tags(trim($_POST['music_link']));
 	$movie_id       	= strip_tags($_POST['movie_id']);
 	$movie_name       = strip_tags(trim($_POST['movie_name']));
 
-	$prepare = $pdo->prepare('INSERT INTO validation_musics (movie_name,movie_id,music_title,,composer,music_link) VALUES (:movie_name,:movie_id,:music_title,:composer,:music_link)');
+	$prepare = $pdo->prepare('INSERT INTO validation_musics (movie_name,movie_id,music_title,composer,music_link) VALUES (:movie_name,:movie_id,:music_title,:composer,:music_link)');
 	$prepare->bindValue('movie_name',$movie_name);
 	$prepare->bindValue('movie_id',$movie_id);
 	$prepare->bindValue('music_title',$music_title);
