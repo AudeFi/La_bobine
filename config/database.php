177d944f91cbb423c74e47235bd6747b1d22124d
CamilleAlv <camille.alvarezdj@gmail.com>
2016-04-14 17:36:45 +0200

    Mail

--- config/database.php
+++ config/database.php
@@ -3,10 +3,15 @@
 define('DB_HOST','localhost');
 define('DB_NAME','si-films');
 define('DB_USER','root');
+<<<<<<< Updated upstream
 
 define('DB_PASS',''); /*Jules*/
 
 /*define('DB_PASS','root');*/
+=======
+// define('DB_PASS',''); /*Jules*/
+define('DB_PASS','root');
+>>>>>>> Stashed changes
 try
 {
     // Try to connect to database
--- config/options.php
+++ config/options.php
@@ -1,9 +1,15 @@
 <?php
 
+<<<<<<< Updated upstream
 //define('URL','http://localhost/La_bobine/');
 //define('URL','http://localhost:8888/La_bobine/');
 // define('URL','http://localhost/La_bobine/');
 define('URL','http://localhost:8888/');
+=======
+// define('URL','http://localhost/La_bobine/');
+//define('URL','http://localhost:8888/');
+define('URL','http://localhost:8888/La_bobine/');
+>>>>>>> Stashed changes
 
 define('SALT','Snp7-1P5f!g');
 
