7f29f3620a4643549b8403f408a413781ba18606
BelamJ <jules.belamy@hotmail.fr>
2016-04-14 19:16:25 +0200

    mention legal link

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,6 +1,8 @@
 
 	</div>
-	
+	<div class="legal-mention" style="float-rigth">
+			<a href="">Mention legales<p></p></a>
+	</div>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 	<script src="<?= URL ?>config/ajax.js"></script>
 	<script src="<?= URL ?>src/js/app/script.js"></script>
