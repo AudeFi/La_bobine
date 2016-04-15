57b36939a530bd44713d3dd7de6e189ade965e1c
maxaloc <maxaloc@gmail.com>
2016-04-15 09:01:14 +0200

    END

---
+++ config/ajax.js
@@ -0,0 +1,44 @@
+// ------------------- AJAX FOR SEARCH SYSTEM
+
+var $container          = $( 'form.search' ),
+    $input_movie_title  = $container.find( 'input#movie_title' ),
+    $input_movie_name = $container.find( 'input#movie_name' ),
+    $input_movie_id     = $container.find( 'input#movie_id' ),
+    $results            = $container.find( '.results' );
+
+$input_movie_title.keyup(function() {
+
+	var value = $.trim( $input_movie_title.val() );
+
+    $.ajax({
+        url     : "config/form-movie.php",
+        type    : "POST",
+        data    : { 'name':value },
+        success : function(data, status, xhr) {
+
+            data = JSON.parse(data);
+            
+            $results.empty();
+
+            for( var i = 0; i < data.length; i++ )
+            {
+                var _data = data[i],
+                $a    = $( '<a>' );
+                $a.addClass('list');
+                $a.data( 'id', _data.id );
+                $a.attr( 'href', '#' );
+                $a.text( _data.title );
+                $a.on( 'click', function(){
+
+                    var $a = $(this);
+                    $input_movie_id.val( $a.data( 'id' ) );
+                    $input_movie_name.val( $a.text() );
+
+                    return false;
+                } );
+
+                $results.append( $a );
+            }
+        },
+    });
+});
